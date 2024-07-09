100b050e68e2c48f057fa065e351d78ff089736e
ZeeAli01 <sheikh.zain0110@gmail.com>
2024-07-10 00:22:28 +0500

    filter issue resolved

--- script.js
+++ script.js
@@ -3,23 +3,27 @@ const rows = [
     topic: "Topic 1",
     duration: "2 hrs",
     link: "https://github.com/Asabeneh/30-Days-Of-JavaScript",
+    hidden: false, //to handle change of table rows
   },
   {
     topic: "Topic 2",
     duration: "1 hr",
     link: "https://github.com/Asabeneh/30-Days-Of-JavaScript",
+    hidden: false,
   },
   {
     topic: "Topic 3",
     duration: "1 hr 45 min",
     link: "https://github.com/Asabeneh/30-Days-Of-JavaScript",
+    hidden: false,
   },
 ];
-
+//add ALL rows to table:
 for (let i = 0; i < rows.length; ++i) {
   const row = document.createElement("tr");
   row.classList.add("row");
-  row.innerHTML = ` <td><input type="checkbox" class="checkbox" value="0"></td><td>${rows[i].topic}</td><td>${rows[i].duration}</td><td><a class="link" href=${rows[i].link} target="_blank">${rows[i].link}</a></td>`;
+  row.innerHTML = ` <td><input type="checkbox" class="checkbox" value="${i}"></td><td>${rows[i].topic}</td><td>${rows[i].duration}</td><td><a class="link" href=${rows[i].link} target="_blank">${rows[i].link}</a></td>`;
+  row.classList.add("task-row");
   const table = document.getElementById("table");
   table.appendChild(row);
 }
@@ -30,37 +34,51 @@ function toggle(source) {
   }
 }
 function hide() {
+  //hide button on screen->
   const checkboxes = document.getElementsByClassName("checkbox");
-  for (var i = 0; i < checkboxes.length; i++) {
+  for (let i = 0; i < checkboxes.length; i++) {
     var checkbox = checkboxes[i];
     var row = checkbox.parentElement.parentElement; //div
-    if (checkbox.checked) {
-      row.classList.add("hidden");
+    if (checkbox.checked === true) {
+      const index = checkbox.value;
+      rows[index].hidden = true;
     } else {
       row.classList.remove("hidden");
     }
   }
+  updateTableState();
 }
-
+//SHOW BUTTON HANDLER:
 function show() {
-  const rows = document.getElementById("table").rows;
-  for (var i = 0; i < rows.length; i++) {
-    rows[i].classList.remove("hidden");
+  //show button on screen->
+  const checkboxes = document.getElementsByClassName("checkbox");
+  for (let i = 0; i < checkboxes.length; i++) {
+    var checkbox = checkboxes[i];
+    if (checkbox.checked === true) {
+      const index = checkbox.value;
+      rows[index].hidden = false;
+    }
   }
+  updateTableState();
 }
+//on load:
 document.addEventListener("DOMContentLoaded", (event) => {
-  // Initial behavior:
+  updateTableState();
   handleChange();
+
+  const outerCheckbox = document.getElementById("outer-checkbox");
+  outerCheckbox.addEventListener("change", (event) => {
+    toggle(event.target);
+  });
+
   const checkboxes = document.querySelectorAll(
     'input[type="checkbox"].checkbox'
   );
-  const outerCheckbox = document.getElementById("outer-checkbox");
-
   for (let i = 0; i < checkboxes.length; i++) {
     checkboxes[i].addEventListener("change", () => {
       let allChecked = true;
-      for (let i = 0; i < checkboxes.length; i++) {
-        if (!checkboxes[i].checked) {
+      for (let j = 0; j < checkboxes.length; j++) {
+        if (!checkboxes[j].checked) {
           allChecked = false;
           break;
         }
@@ -84,5 +102,41 @@ function handleChange() {
   } else {
     showBtn.classList.add("hidden");
     hideBtn.classList.add("hidden");
+  }
+  updateTableState();
+}
+
+function updateTableState() {
+  const table = document.getElementById("table");
+  const select = document.getElementById("select");
+  const colHeading = `
+      <tr id="column-name">
+        <th><input type="checkbox" id="outer-checkbox" onclick="toggle(this)"></th>
+        <th>Title</th>
+        <th>Duration</th>
+        <th>Link</th>
+      </tr>
+    `;
+  table.innerHTML = colHeading;
+
+  for (let i = 0; i < rows.length; i++) {
+    const tr = document.createElement("tr");
+    tr.classList.add("row");
+    tr.innerHTML = `
+        <td><input type="checkbox" class="checkbox" value="${i}"></td>
+        <td>${rows[i].topic}</td>
+        <td>${rows[i].duration}</td>
+        <td><a class="link" href="${rows[i].link}" target="_blank">${rows[i].link}</a></td>
+      `;
+
+    if (select.value === "show" && rows[i].hidden) {
+      // Don't show hidden rows when the filter is "show"
+      tr.classList.add("hidden");
+    } else if (select.value === "hide" && !rows[i].hidden) {
+      // Don't show visible rows when the filter is "hide"
+      tr.classList.add("hidden");
+    }
+
+    table.appendChild(tr);
   }
 }
