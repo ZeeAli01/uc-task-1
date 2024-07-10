14ab1d907861970e442b4de9de137a980760682c
ZeeAli01 <sheikh.zain0110@gmail.com>
2024-07-11 00:11:31 +0500

    validations

--- script.js
+++ script.js
@@ -24,14 +24,25 @@ document.addEventListener("DOMContentLoaded", () => {
   handleChange(); //show filter shows HIDE btn and vice versa.
   //new:
   //if some outer checkbox change then immediately change all of inner ones:
+  //----------------TASK 2--------------------
+  const form = document.getElementById("modal-form");
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    // Capture the form data:
+    const data = new FormData(form);
+    console.log(data.get("title-input"));
+    console.log(data.get("duration-input"));
+    console.log(data.get("url-input"));
+  });
+  //----------------TASK 2--------------------
   document
-    .getElementById("outer-checkbox")
+    .getElementById("outer-checkbox-task1")
     .addEventListener("change", function (event) {
       toggle(event.target);
     });
 
   const checkboxes = document.querySelectorAll(
-    'input[type="checkbox"].checkbox'
+    'input[type="checkbox"].checkbox-task1'
   );
   //if some inner checkbox changes then immediately change master one:
   for (let i = 0; i < checkboxes.length; i++) {
@@ -40,10 +51,10 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 function addRowsToTable() {
-  const table = document.getElementById("table");
+  const table = document.getElementById("table-task1");
   const colHeading = `
-      <tr id="column-name">
-          <th><input type="checkbox" id="outer-checkbox" onclick="toggle(this)"></th>
+      <tr id="column-name-task1">
+          <th><input type="checkbox" id="outer-checkbox-task1" onclick="toggle(this)"></th>
           <th>Title</th>
           <th>Duration</th>
           <th>Link</th>
@@ -53,18 +64,20 @@ function addRowsToTable() {
 
   for (let i = 0; i < rows.length; ++i) {
     const row = document.createElement("tr");
-    row.classList.add("row");
+    row.classList.add("row-task1");
     row.innerHTML = `
-      <td><input type="checkbox" class="checkbox" value="${i}"></td>
+      <td><input type="checkbox" class="checkbox-task1" value="${i}"></td>
       <td>${rows[i].topic}</td>
       <td>${rows[i].duration}</td>
-      <td><a class="link" href=${rows[i].link} target="_blank">${rows[i].link}</a></td>`;
-    row.classList.add("task-row");
+      <td><a class="link-task1" href=${rows[i].link} target="_blank">${rows[i].link}</a></td>`;
+    row.classList.add("task-row-task1");
     table.appendChild(row);
   }
 }
 function toggle(source) {
-  let checkboxes = document.querySelectorAll('input[type="checkbox"].checkbox');
+  let checkboxes = document.querySelectorAll(
+    'input[type="checkbox"].checkbox-task1'
+  );
   for (let i = 0; i < checkboxes.length; i++) {
     checkboxes[i].checked = source.checked;
   }
@@ -73,7 +86,7 @@ function toggle(source) {
 }
 //on click hide button:
 function hide() {
-  const checkboxes = document.getElementsByClassName("checkbox");
+  const checkboxes = document.getElementsByClassName("checkbox-task1");
   for (let i = 0; i < checkboxes.length; i++) {
     if (checkboxes[i].checked) {
       const index = checkboxes[i].value;
@@ -84,7 +97,7 @@ function hide() {
 }
 //on click show button:
 function show() {
-  const checkboxes = document.getElementsByClassName("checkbox");
+  const checkboxes = document.getElementsByClassName("checkbox-task1");
   for (let i = 0; i < checkboxes.length; i++) {
     if (checkboxes[i].checked) {
       const index = checkboxes[i].value;
@@ -95,29 +108,29 @@ function show() {
 }
 //show and hide buttons:
 function handleChange() {
-  const showBtn = document.getElementById("show-btn");
-  const hideBtn = document.getElementById("hide-btn");
-  const select = document.getElementById("select");
+  const showBtn = document.getElementById("show-btn-task1");
+  const hideBtn = document.getElementById("hide-btn-task1");
+  const select = document.getElementById("select-task1");
 
   if (select.value === "show") {
-    hideBtn.classList.remove("hidden");
-    showBtn.classList.add("hidden");
+    hideBtn.classList.remove("hidden-task1");
+    showBtn.classList.add("hidden-task1");
   } else if (select.value === "hide") {
-    hideBtn.classList.add("hidden");
-    showBtn.classList.remove("hidden");
+    hideBtn.classList.add("hidden-task1");
+    showBtn.classList.remove("hidden-task1");
   } else {
-    showBtn.classList.add("hidden");
-    hideBtn.classList.add("hidden");
+    showBtn.classList.add("hidden-task1");
+    hideBtn.classList.add("hidden-task1");
   }
   updateTableState();
 }
 //print rows based on hidden property:
 function updateTableState() {
-  const table = document.getElementById("table");
-  const select = document.getElementById("select");
+  const table = document.getElementById("table-task1");
+  const select = document.getElementById("select-task1");
   const colHeading = `
-      <tr id="column-name">
-          <th><input type="checkbox" id="outer-checkbox" onclick="toggle(this)"></th>
+      <tr id="column-name-task1">
+          <th><input type="checkbox" id="outer-checkbox-task1" onclick="toggle(this)"></th>
           <th>Title</th>
           <th>Duration</th>
           <th>Link</th>
@@ -133,18 +146,18 @@ function updateTableState() {
       continue;
     } else {
       const tr = document.createElement("tr");
-      tr.classList.add("row");
+      tr.classList.add("row-task1");
       tr.innerHTML = `
-          <td><input type="checkbox" class="checkbox" value="${i}"></td>
+          <td><input type="checkbox" class="checkbox-task1" value="${i}"></td>
           <td>${row.topic}</td>
           <td>${row.duration}</td>
-          <td><a class="link" href="${row.link}" target="_blank">${row.link}</a></td>
+          <td><a class="link-task1" href="${row.link}" target="_blank">${row.link}</a></td>
       `;
       table.appendChild(tr);
     }
   }
   const checkboxes = document.querySelectorAll(
-    'input[type="checkbox"].checkbox'
+    'input[type="checkbox"].checkbox-task1'
   );
   //if some inner checkbox changes then immediately change master one:
   for (let i = 0; i < checkboxes.length; i++) {
@@ -157,9 +170,9 @@ function updateMasterCheckbox() {
   let allChecked = true;
   let anyChecked = false;
   const checkboxes = document.querySelectorAll(
-    'input[type="checkbox"].checkbox'
+    'input[type="checkbox"].checkbox-task1'
   );
-  const outerCheckbox = document.getElementById("outer-checkbox");
+  const outerCheckbox = document.getElementById("outer-checkbox-task1");
 
   for (let i = 0; i < checkboxes.length; i++) {
     if (checkboxes[i].checked) {
@@ -177,3 +190,30 @@ function updateMasterCheckbox() {
   }
   outerCheckbox.checked = allChecked;
 }
+
+//Task 2:
+
+//create:
+
+// const submitButton = document.getElementById("submit-button");
+// submitButton.addEventListener("click", function (event) {
+//   event.preventDefault();
+//   const form = document.getElementById("modal-form");
+//   const data = new FormData(form);
+//   console.log(data.get("title-input"));
+//   console.log(data.get("duration-input"));
+//   console.log(data.get("url-input"));
+//   document.getElementById("exampleModal").classList.remove("fade");
+// });
+// Handle form submission
+// const submitButton = document.getElementById("submit-button");
+// submitButton.addEventListener("click", function (event) {
+//   event.preventDefault();
+
+//   // Capture the form data
+//   const form = document.getElementById("modal-form");
+//   const data = new FormData(form);
+//   console.log(data.get("title-input"));
+//   console.log(data.get("duration-input"));
+//   console.log(data.get("url-input"));
+// });
